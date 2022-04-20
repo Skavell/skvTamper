3e78da7ba5a5029ec7c66830fc63c187f73c11b3
Skavell <104095918+Skavell@users.noreply.github.com>
2022-04-20 23:23:42 +0500

    Update tamper.user.js

--- tamper.user.js
+++ tamper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SskAavegotchi V4.1
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.1
 // @description  try to take over the world!
 // @author       You
 // @match        https://verse.aavegotchi.com/*
